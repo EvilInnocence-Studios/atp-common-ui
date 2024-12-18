886eb77cd4369813b99df030056366959cafe9f9
Andrew Wittrock <andy@wittrock.us>
2024-12-18 08:03:21 -0600

    Move homepage route to admin module

--- lib/routes.tsx
+++ lib/routes.tsx
@@ -1,9 +1,7 @@
 import { TagManager } from "@common/components/TagManager";
-import { HomePage } from "@admin/components/HomePage";
 
 export const commonRoutes = {
     admin: [
-        {path: "/", component: HomePage},
         {path: "/tags", component: TagManager},
     ]
 }
