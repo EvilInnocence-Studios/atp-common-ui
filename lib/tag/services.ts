d075aef316cd6edf068eae81c910b47d296040c5
Andrew Wittrock <andy@wittrock.us>
2025-01-04 22:08:34 -0600

    Add get all tag service

--- lib/tag/services.ts
+++ lib/tag/services.ts
@@ -9,6 +9,7 @@ export const tagGroupServices = ({get, post, /*put,*/ patch, remove}: IMethods) 
         update: (id: number, data: Partial<ITagGroup>) => patch(`group/${id}`, data),
         remove: (id: number) => remove(`group/${id}`),
         tag: {
+            getAll: ():Promise<ITag[]> => get('tag').then(getResults<ITag[]>),
             search: (tagGroupId: number):Promise<ITag[]> => get(`group/${tagGroupId}/tag`).then(getResults<ITag[]>),
             create: (tagGroupId: number, tag:Partial<ITag>) => post(`group/${tagGroupId}/tag`, tag),
             update: (tagGroupId: number, tagId: number, data: Partial<ITag>) => patch(`group/${tagGroupId}/tag/${tagId}`, data),
