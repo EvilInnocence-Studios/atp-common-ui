bcdc342c07935d8b142b14445c4ae16060fde4be
Andrew Wittrock <andy@wittrock.us>
2025-01-09 14:18:53 -0600

    Add links to banners

--- components/Banner/Banner.component.tsx
+++ components/Banner/Banner.component.tsx
@@ -3,17 +3,20 @@ import { BannerImage } from "../BannerImage";
 import {BannerProps} from "./Banner.d";
 import styles from './Banner.module.scss';
 import clsx from "clsx";
+import { Link } from "react-router";
 
 export const BannerComponent = ({banner}:BannerProps) => 
-    <div className={styles.banner}>
-        <div className={styles.info}>
-            <h1>{banner.title}</h1>
-            <p>{banner.description}</p>
+    <Link to={banner.link}>
+        <div className={styles.banner}>
+            <div className={styles.info}>
+                <h1>{banner.title}</h1>
+                <p>{banner.description}</p>
+            </div>
+            {!!banner.buttonText && <div className={clsx([styles.button, banner.buttonLocation && styles[banner.buttonLocation]])}>
+                <Button type="primary" size="large">
+                    {banner.buttonText}
+                </Button>
+            </div>}
+            <BannerImage bannerId={banner.id} />
         </div>
-        {!!banner.buttonText && <div className={clsx([styles.button, banner.buttonLocation && styles[banner.buttonLocation]])}>
-            <Button type="primary" size="large">
-                {banner.buttonText}
-            </Button>
-        </div>}
-        <BannerImage bannerId={banner.id} />
-    </div>;
+    </Link>;
