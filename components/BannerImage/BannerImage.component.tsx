ad9ba79baf73713d347abafdea7e495d90174ed9
Andrew Wittrock <andy@wittrock.us>
2025-01-30 07:58:49 -0600

    Add loading spinner to banner images

--- components/BannerImage/BannerImage.component.tsx
+++ components/BannerImage/BannerImage.component.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
 import {BannerImageProps} from "./BannerImage.d";
 import styles from './BannerImage.module.scss';
+import { Spin } from "antd";
 
 export const imgHost = `https://evilinnocence.s3.us-east-1.amazonaws.com/media/banner/`;
 
 
-export const BannerImageComponent = ({banner, isLoading}:BannerImageProps) =>
+export const BannerImageComponent = ({banner, isLoading}:BannerImageProps) => <Spin spinning={isLoading}>
     <img
         src={banner
             ? `${imgHost}${banner.url}`
@@ -14,3 +15,4 @@ export const BannerImageComponent = ({banner, isLoading}:BannerImageProps) =>
         alt={banner?.title}
         className={clsx([styles.bannerImage, isLoading && styles.loading])}
     />
+</Spin>;
--- components/BannerImage/BannerImage.container.ts
+++ components/BannerImage/BannerImage.container.ts
@@ -1,21 +1,21 @@
-import { createInjector, inject, mergeProps } from "unstateless";
-import {BannerImageComponent} from "./BannerImage.component";
-import {IBannerImageInputProps, BannerImageProps, IBannerImageProps} from "./BannerImage.d";
-import { useEffect, useState } from "react";
 import { IBanner } from "@common-shared/banner/types";
-import { useLoader } from "@core/lib/useLoader";
 import { services } from "@core/lib/api";
+import { useLoaderAsync } from "@core/lib/useLoader";
+import { useEffect, useState } from "react";
+import { createInjector, inject, mergeProps } from "unstateless";
+import { BannerImageComponent } from "./BannerImage.component";
+import { BannerImageProps, IBannerImageInputProps, IBannerImageProps } from "./BannerImage.d";
 
 const injectBannerImageProps = createInjector(({bannerId}:IBannerImageInputProps):IBannerImageProps => {
     const [banner, setBanner] = useState<IBanner | null>(null);
-    const loader = useLoader();
+    const loader = useLoaderAsync();
 
     useEffect(() => {
         if(bannerId) {
-            loader.start();
-            services().banner.get(bannerId)
-                .then(setBanner)
-                .finally(loader.stop);
+            loader(async () =>
+                services().banner.get(bannerId)
+                    .then(setBanner)
+            );
         }
     }, [bannerId]);
 
