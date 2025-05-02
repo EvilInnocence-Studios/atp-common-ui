512d88b16f59bde8d0625f27657c0c0be0a5a53e
Andrew Wittrock <andy@wittrock.us>
2025-05-01 22:53:55 -0500

    Memoize settings fetch

--- lib/setting/services.ts
+++ lib/setting/services.ts
@@ -3,14 +3,16 @@ import { services } from "@core/lib/api";
 import { IMethods } from "@core/lib/types";
 import { getResults } from "@core/lib/util";
 import { useEffect } from "react";
+import { memoize, memoizePromise } from "ts-functional";
 import { Index } from "ts-functional/dist/types";
 import { useSharedState } from "unstateless";
 
 const useSettingsRaw = useSharedState<Index<string>>('settings', {});
 
-export const settingServices = ({ get, post, /*put,*/ patch, remove }: IMethods) => ({
+export const settingServices = memoize(({ get, post, put, patch, remove }: IMethods) => ({
     setting: {
         search: (): Promise<ISetting[]> => get(`setting`).then(getResults),
+        searchMemoized: memoizePromise((): Promise<ISetting[]> => get(`setting`).then(getResults)),
         create: (key: string, value: any): Promise<ISetting> => post(`setting`, { key, value }),
         update: (id: string, value: any): Promise<ISetting> => patch(`setting/${id}`, { value }),
         remove: (id: string) => remove(`setting/${id}`),
@@ -19,22 +21,25 @@ export const settingServices = ({ get, post, /*put,*/ patch, remove }: IMethods)
                 return Promise.resolve(useSettingsRaw.getValue()[key]);
             }
             // Load all settings and cache them onto the target
-            return get(`setting`).then(getResults).then((settings:ISetting[]) => {
-                const newSettings:Index<string> = {};
-                settings.forEach(({ key, value }) => {
-                    newSettings[key] = value;
+            return settingServices({get, post, put, patch, remove}).setting.searchMemoized()
+                .then((settings:ISetting[]) => {
+                    const newSettings:Index<string> = {};
+                    settings.forEach(({ key, value }) => {
+                        newSettings[key] = value;
+                    });
+                    useSettingsRaw.setValue(newSettings);
+                    return newSettings[key];
                 });
-                useSettingsRaw.setValue(newSettings);
-                return newSettings[key];
-            });
         }
     },
-});
+}), {});
 
 export const useSetting = (key:string):string => {
     const [settings] = useSettingsRaw();
 
-    useEffect(() => {services().setting.get(key)}, []);
+    useEffect(() => {
+        services().setting.get(key)
+    }, []);
 
     return settings[key];
 }
