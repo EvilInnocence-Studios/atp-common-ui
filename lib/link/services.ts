bba994da54661d1fbfeeb24ecefb1a768bddd49b
Andrew Wittrock <andy@wittrock.us>
2025-08-23 10:12:21 -0500

    Add link services

---
+++ lib/link/services.ts
@@ -0,0 +1,20 @@
+import { ILink, ILinkList, NewLinkList } from "@common-shared/link/types";
+import { IMethods } from "@core/lib/types";
+import { getResults } from "@core/lib/util";
+
+export const linkServices = ({get, post, /*put,*/ patch, remove}: IMethods) => ({
+    linkList: {
+        create: (linkList: NewLinkList) => post('linkList', linkList).then(getResults<ILinkList>),
+        search: (query?:{}):Promise<ILinkList[]> => get('linkList', query).then(getResults<ILinkList[]>),
+        update: (id:string, data: Partial<ILinkList>) => patch(`linkList/${id}`, data),
+        remove: (id:string) => remove(`linkList/${id}`),
+        link: {
+            get: (listId:string, linkId:string):Promise<ILink> => get(`linkList/${listId}/link/${linkId}`).then(getResults<ILink>),
+            search: (listId:string):Promise<ILink[]> => get(`linkList/${listId}/link`).then(getResults<ILink[]>),
+            create: (listId:string, link:Partial<ILink>) => post(`linkList/${listId}/link`, link),
+            update: (listId:string, linkId:string, data: Partial<ILink>) => patch(`linkList/${listId}/link/${linkId}`, data),
+            remove: (listId:string, linkId:string) => remove(`linkList/${listId}/link/${linkId}`),
+            sort: (listId:string, linkId:string, newIndex:number) => post(`linkList/${listId}/link/sort`, {linkId, newIndex}),
+        }
+    }
+});
--- lib/services.ts
+++ lib/services.ts
@@ -4,12 +4,14 @@ import { synonymServices } from "./synonym/services";
 import { tagGroupServices } from "./tag/services";
 import { settingServices } from "./setting/services";
 import { getResults } from "@core/lib/util";
+import { linkServices } from "./link/services";
 
 export const commonServices = (methods:IMethods) => ({
     ...tagGroupServices(methods),
     ...synonymServices(methods),
     ...bannerServices(methods),
     ...settingServices(methods),
+    ...linkServices(methods),
     cache: {
         clear: (cacheType:string) => methods.post(`cache/${cacheType}`),
     },
