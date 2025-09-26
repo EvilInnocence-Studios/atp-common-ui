3a75b8d06e5a3f091508d6974f71eba7e75d12d9
Andrew Wittrock <andy@wittrock.us>
2025-09-26 00:44:57 -0500

    Move sitePlugins to common module

--- index.tsx
+++ index.tsx
@@ -1,16 +1,20 @@
 import { IModule } from "@core/lib/module";
-import { sitePlugins } from "@public/index";
 import { BannerList } from "./components/BannerList";
 import { commonMenus } from "./lib/menus";
 import { commonRoutes } from "./lib/routes";
 import { useSetting } from "./lib/setting/services";
 import { commonSettings } from "./lib/settings";
+import { rendererPlugins } from "@core/lib/plugins/renderer";
 
 export const module:IModule = {
     name: "common",
     menus: commonMenus,
     routes: commonRoutes,
     settings: commonSettings,
+};
+
+export const sitePlugins = {
+    homepage: rendererPlugins(),
 };
 
 sitePlugins.homepage.register(99, () => {
