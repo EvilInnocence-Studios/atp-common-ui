e5f800889c6f5f1fc425a872cdcc8f9ae04400a3
Andrew Wittrock <andy@wittrock.us>
2025-09-26 21:55:40 -0500

    Add theme settings

---
+++ lib/settings/theme/text.ts
@@ -0,0 +1,36 @@
+import { ISettingsModule } from "@common/lib/setting/types";
+
+export const textSettings:ISettingsModule = {
+    Text: {
+        "theme.textSizeDefault": {
+            displayName: "Default Text Size",
+            type: "string",
+            defaultValue: 14,
+            description: "The default size for text.",
+        },
+        "theme.textSizeSmall": {
+            displayName: "Small Text Size",
+            type: "string",
+            defaultValue: 12,
+            description: "The size for small text.",
+        },
+        "theme.textSizeLarge": {
+            displayName: "Large Text Size",
+            type: "string",
+            defaultValue: 18,
+            description: "The size for large text.",
+        },
+        "theme.textSizeXS": {
+            displayName: "Extra Small Text Size",
+            type: "string",
+            defaultValue: 10,
+            description: "The size for extra small text.",
+        },
+        "theme.textSizeXL": {
+            displayName: "Extra Large Text Size",
+            type: "string",
+            defaultValue: 22,
+            description: "The size for extra large text.",
+        },
+    }
+}
