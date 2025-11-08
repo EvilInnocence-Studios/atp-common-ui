ec82556367cf9ef23184dc9683270ecf5bfd3e99
Andrew Wittrock <andy@wittrock.us>
2025-11-08 10:34:08 -0600

    Remove custom styling from link list submenus

--- components/LinkList/LinkList.component.tsx
+++ components/LinkList/LinkList.component.tsx
@@ -10,11 +10,13 @@ import styles from "./LinkList.module.scss";
 export const LinkListComponent = ({links, isLoading, className}:LinkListProps) =>
     <Spin spinning={isLoading}>
         <ul className={clsx([`linkList`, className])}>
-            {links.sort(sort.by(prop<ILink, "order">("order")).asc).map(link => <li key={link.id} className={clsx([styles.linkItem, "linkItem"])}>
-                <Link to={link.url}>
-                    {link.text}
-                </Link>
-                {link.subMenuKey && <LinkList id={link.subMenuKey} className={styles.subMenu} />}
-            </li>)}
+            {links.sort(sort.by(prop<ILink, "order">("order")).asc).map(link =>
+                <li key={link.id} className={clsx([styles.linkItem, "linkItem"])}>
+                    <Link to={link.url}>
+                        {link.text}
+                    </Link>
+                    {link.subMenuKey && <LinkList id={link.subMenuKey} className={clsx([styles.subMenu, "subMenu"])} />}
+                </li>
+            )}
         </ul>
     </Spin>;
