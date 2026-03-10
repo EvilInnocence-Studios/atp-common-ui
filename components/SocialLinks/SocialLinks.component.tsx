569d1db89f3c7ca8f9fd429185772ce5905b72ad
Andrew Wittrock <andy@wittrock.us>
2026-03-10 00:50:26 -0500

    Replace social link icons

--- components/SocialLinks/SocialLinks.component.tsx
+++ components/SocialLinks/SocialLinks.component.tsx
@@ -1,21 +1,52 @@
 import { overridable } from "@core/lib/overridable";
-import { faBluesky, faFacebook, faInstagram, faPatreon, faTiktok, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import clsx from "clsx";
+import SVG from "react-inlinesvg";
 import { SocialLinksProps } from "./SocialLinks.d";
 import styles from './SocialLinks.module.scss';
+
+interface IconProps {
+    slug: string;
+    variant?: string;
+    width?: number;
+    height?: number;
+}
+
+const Icon = ({slug, variant, width, height}:IconProps) => 
+    <SVG
+        src={`https://cdn.jsdelivr.net/gh/GLINCKER/thesvg@main/public/icons/${slug}/${variant || "mono"}.svg`}
+        width={width}
+        height={height}
+        className={styles.icon}
+    />
+
 
 export const SocialLinksComponent = overridable(({ facebook, twitter, blueSky, instagram, patreon, youtube, tikTok, kofi, classes = styles }: SocialLinksProps) =>
     <div className={clsx([classes.socialLinks, "socialLinks"])}>
         <div className={classes.links}>
-            {blueSky && <a href={`https://bsky.app/profile/${blueSky}`} target="_blank"><FontAwesomeIcon icon={faBluesky} /></a>}
-            {twitter && <a href={`https://twitter.com/${twitter}`} target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>}
-            {facebook && <a href={`https://www.facebook.com/${facebook}`} target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>}
-            {instagram && <a href={`https://www.instagram.com/${instagram}`} target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>}
-            {kofi && <a href={`https://ko-fi.com/${kofi}`} target="_blank"><img src="/kofi_symbol.png" /></a>}
-            {patreon && <a href={`https://www.patreon.com/${patreon}`} target="_blank"><FontAwesomeIcon icon={faPatreon} /></a>}
-            {youtube && <a href={`https://www.youtube.com/${youtube}`} target="_blank"><FontAwesomeIcon icon={faYoutube} /></a>}
-            {tikTok && <a href={`https://www.tiktok.com/${tikTok}`} target="_blank"><FontAwesomeIcon icon={faTiktok} /></a>}
+            {blueSky && <a href={`https://bsky.app/profile/${blueSky}`} target="_blank">
+                <Icon slug="bluesky" />
+            </a>}
+            {twitter && <a href={`https://www.twitter.com/${twitter}`} target="_blank">
+                <Icon slug="x" />
+            </a>}
+            {facebook && <a href={`https://www.facebook.com/${facebook}`} target="_blank">
+                <Icon slug="facebook" />
+            </a>}
+            {instagram && <a href={`https://www.instagram.com/${instagram}`} target="_blank">
+                <Icon slug="instagram" />
+            </a>}
+            {kofi && <a href={`https://ko-fi.com/${kofi}`} target="_blank">
+                <Icon slug="ko-fi"/>
+            </a>}
+            {patreon && <a href={`https://www.patreon.com/${patreon}`} target="_blank">
+                <Icon slug="patreon" />
+            </a>}
+            {youtube && <a href={`https://www.youtube.com/${youtube}`} target="_blank">
+                <Icon slug="youtube" />
+            </a>}
+            {tikTok && <a href={`https://www.tiktok.com/${tikTok}`} target="_blank">
+                <Icon slug="tiktok" />
+            </a>}
         </div>
     </div>
 );
