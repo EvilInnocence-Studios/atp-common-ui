2139d61c1e22a2aceaa7d25cc1ffe13cbce37841
Andrew Wittrock <andy@wittrock.us>
2026-04-22 12:58:39 -0500

    Remove snippet component

--- components/Snippet/Snippet.component.tsx
+++
@@ -1,7 +0,0 @@
-import { overridable } from "@core/lib/overridable";
-import {SnippetProps} from "./Snippet.d";
-import styles from './Snippet.module.scss';
-
-export const SnippetComponent = overridable(({classes = styles}:SnippetProps) =>
-    <div>Snippet component goes here.</div>
-);
